22f3f294c7833e93cc5fc0bba41c35894acbeb3f
Vidit Shah <vidit0210@protonmail.com>
2023-03-12 14:51:34 +0530

    Layout Setup

---
+++ app/SideNavigation.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const SideNavigation = (props: Props) => {
+  return <div className="border-2 border-orange-300">Side Navigation</div>;
+};
+
+export default SideNavigation;
---
+++ app/api/TopNavigation.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const TopNavigation = (props: Props) => {
+  return <div className="border-2 border-red-500">TopNavigation</div>;
+};
+
+export default TopNavigation;
